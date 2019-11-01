40a45d5ac400387c7700f03aa161af9bec9172cb
qn1994824 <1348318967@qq.com>
2019-11-01 10:08:16 +0800

    11.1.10

--- note-talking-app/src/App.js
+++ note-talking-app/src/App.js
@@ -91,7 +91,7 @@ class App extends Component {
                     <Route path="/make" component={Make}></Route>
                     <Route path="/keep" component={Keep}></Route>
                     <Route path="/test" component={Test}></Route>
-                    <Route path="/case" component={Case}></Route>
+					<Route path="/case" component={Case}></Route>
 					<Route path="/addone" component={Addone}></Route>
                     <Route path="/" component={Not}></Route>
                 </Switch>
