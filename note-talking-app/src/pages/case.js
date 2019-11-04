c326caf59cfb71020e06a62c41497042d8945b75
qn1994824 <1348318967@qq.com>
2019-11-04 15:30:22 +0800

    case

---
+++ note-talking-app/src/pages/case.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/home.css';
+import { Icon } from 'antd';
+
+class Case extends React.Component {
+    render() {
+		<div>787878</div>
+}
+}
+export default Case;
